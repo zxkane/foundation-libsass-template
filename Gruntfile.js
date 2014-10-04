22fc7dcaa4f98a00e3cd40c40b28dadd666abc19
Kane Zhu <kane.mx@gmail.com>
2014-10-04 09:30:35 +0800

    Global style for assistant.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          '../billiards/static/css/app.css': 'scss/app.scss'
         }        
       }
     },
