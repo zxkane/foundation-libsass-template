2174e5e3ba787ce66a6b12eb4414a830e67d196e
Kane Zhu <kane.mx@gmail.com>
2014-10-11 13:39:13 +0800

    Add stylesheet of detail page.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          '../billiards/static/css/app.css': 'scss/app.scss'
+          '../billiards/static/css/app.css': 'scss/app.scss',
+          '../billiards/static/css/escort/detail.css': 'scss/detail.scss'
         }        
       }
     },
